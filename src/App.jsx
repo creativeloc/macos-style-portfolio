97cb591a76fc6ad09243dcc89fd805812be3d5f5
Lawrence Caulker <locaulker@gmail.com>
2026-07-17 15:28:17 -0400

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to My MacFolio</h1>
+    </div>
+  )
+}
+export default App
